211e7aa78da9e734801a2a8982a2158f1530f846
文期 <sjs.sjs@alibaba-inc.com>
2017-09-02 00:04:51 +0800

    feat: init project

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1504281713204_8738';
+
+  // add your config here
+
+  return config;
+};
