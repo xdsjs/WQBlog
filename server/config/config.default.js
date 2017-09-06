716430b7cc7571ed492d1d7c8cdfbe4b23afa0fe
文期 <sjs.sjs@alibaba-inc.com>
2017-09-06 22:47:43 +0800

    fix: 修改目录结构

---
+++ server/config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1504709084643_3813';
+
+  // add your config here
+
+  return config;
+};
