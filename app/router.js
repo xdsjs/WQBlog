1ff89e338ab12a4ae2ea80853668b968ffa7c45a
文期 <sjs.sjs@alibaba-inc.com>
2017-09-06 22:33:28 +0800

    fix: delete all

--- app/router.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  app.get('/', 'home.index');
-  app.get('/admin/login', app.controller.user.login);
-};
--- config/config.default.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.keys = this.name + '_1504281713204_8738';
-
-exports.mongoose = {
-  url: 'mongodb://127.0.0.1/WQBlog',
-  options: {},
-};
